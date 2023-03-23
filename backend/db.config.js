d5cddca92a91be7d2ed18e330e03d32c76b2238f
Delafosse Alexandre <alex.delaf@outlook.fr>
2023-03-23 16:51:43 +0100

    exercice 5

--- backend/db.config.js
+++ backend/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   HOST: "mysql",
   USER: "root",
-  PASSWORD: "root",
-  DATABASE: "todolist",
+  PASSWORD: process.env.MYSQL_ROOT_PASSWORD,
+  DATABASE: process.env.MYSQL_DATABASE,
   PORT: "3306",
 };
